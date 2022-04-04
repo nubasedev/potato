0f588aa6371a442286df1736bc5e71e368aa14a7
Km.Van <kmvan.com@gmail.com>
2022-04-04 10:47:08 +0800

    Update ReactMde.tsx #323

--- src/components/ReactMde.tsx
+++ src/components/ReactMde.tsx
@@ -228,7 +228,12 @@ export class ReactMde extends React.Component<ReactMdeProps, ReactMdeState> {
           writeButtonProps={finalChildProps.writeButton}
           previewButtonProps={finalChildProps.previewButton}
         />
-        <div className={classNames({ invisible: selectedTab !== "write" })}>
+        <div
+          className={classNames(
+            { invisible: selectedTab !== "write" },
+            "mde-invisible-wrapper"
+          )}
+        >
           <TextArea
             classes={classes?.textArea}
             suggestionsDropdownClasses={classes?.suggestionsDropdown}
