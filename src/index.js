c4f31e9dab388404ec641c151003b5e3a828762a
Km.Van <kmvan.com@gmail.com>
2023-08-09 11:27:06 +0800

    remove .js files in src dir

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import { getDefaultCommandMap, getDefaultToolbarCommands, } from './commands/default-commands/defaults';
-import { Mde } from './components/bootstrap';
-import { selectWord } from './utils/markdown-util';
-export { Mde, getDefaultCommandMap, getDefaultToolbarCommands, selectWord };
--- src/utils/insert-text-at-position.js
+++
@@ -1,7 +0,0 @@
-/**
- * Inserts the given text at the cursor. If the element contains a selection, the selection
- * will be replaced by the text.
- */
-export const insertText = (input, text) => {
-    input.setRangeText(text, input.selectionStart || 0, input.selectionEnd || 0, 'select');
-};
