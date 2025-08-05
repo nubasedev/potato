0134faf5399bcc96cf8ce9e505bb9251096c8295
Andre Pena <andrerpena@gmail.com>
2025-08-05 21:44:20 +0100

    Transform to @nubasedev/potato with minimal package.json

--- src/commands/markdown-commands/orderedListCommand.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "../command";
-import { makeList } from "../../helpers/listHelpers";
-
-export const orderedListCommand: Command = {
-  execute: ({ initialState, textApi }) => {
-    makeList(initialState, textApi, (item, index) => `${index + 1}. `);
-  }
-};
--- src/commands/markdown-commands/unorderedListCommand.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "../command";
-import { makeList } from "../../helpers/listHelpers";
-
-export const unorderedListCommand: Command = {
-  execute: ({ initialState, textApi }) => {
-    makeList(initialState, textApi, "- ");
-  }
-};
